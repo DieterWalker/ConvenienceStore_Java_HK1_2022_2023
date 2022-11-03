55dc75fa7e64d6ffc8267840c8f3985c9ebfb4a8
Dieter Walker <103620023+DieterWalker@users.noreply.github.com>
2022-11-03 08:41:41 +0700

    Delete GUI directory

--- GUI/Table/MyTable.java
+++
@@ -1,8 +0,0 @@
-package GUI.Table;
-
-import GUI.ManagerScreen;
-import javax.swing.table.DefaultTableModel;
-
-public abstract class MyTable extends DefaultTableModel {
-    public abstract void show();
-}
